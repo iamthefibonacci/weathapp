80c85aa949ad5b0e5945beac1fcbea3889a13e4b
McWilton <mcwilton85@gmail.com>
2020-11-16 20:38:25 +0200

    Update forms.py

--- weatherapi/forms.py
+++ weatherapi/forms.py
@@ -8,6 +8,6 @@ class WeatherForm(ModelForm):
         model = Weather
         fields = ['city', 'period']
         widgets = {
-            'city': TextInput(attrs={'class': 'input', 'placeholder': 'City Name', 'required': True}),
-            'period': NumberInput(attrs={'class': 'input', 'min': 1, 'max': 3, 'required': True, 'type': 'number'}),
+            'city': TextInput(attrs={'class': 'input', 'placeholder': 'City', 'required': True}),
+            'period': NumberInput(attrs={'class': 'input', 'placeholder': 'Period','min': 1, 'max': 3, 'required': True, 'type': 'number'}),
         }
