ee5745c0dd89ce62254c1010036a2f681cf739f9
McWilton <mcwilton85@gmail.com>
2020-11-16 19:36:27 +0200

    Update models.py

--- weatherapi/models.py
+++ weatherapi/models.py
@@ -8,19 +8,3 @@ class Weather(models.Model):
     class Meta:
         verbose_name_plural = 'weather'
 
-
-class Temperature(models.Model):
-    period = models.DateField()
-    temperature = models.IntegerField()
-    humidity = models.IntegerField()
-
-
-class Values(models.Model):
-    min = models.IntegerField()
-    max = models.IntegerField()
-    ave = models.IntegerField()
-    med = models.IntegerField()
-
-
-class Humidity(models.Model):
-    humidity = models.IntegerField()
